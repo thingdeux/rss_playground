e48a036bef82e2cb07c2f287dd798223c53b73f4
Joshua Johnson <me@josh.land>
2015-04-10 01:13:56 -0700

    Moved heroku specific info to settings.py

--- mediaplayground/local_settings.py
+++ mediaplayground/local_settings.py
@@ -4,21 +4,4 @@ import settings
 # Uploading to the repo for the sake of easy consumption ...
 SECRET_KEY = '++)pn2(4q7g@@05y^pfp2c7f_mn&3=^uhxl9&d!7od4fpi(hej'
 
-# Parse database configuration from $DATABASE_URL
-settings.DATABASES['default'] = dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
-
-# Static asset configuration
-import os
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = 'staticfiles'
-STATIC_URL = '/static/'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
