4346f7f1efe0f684fcca8f86939d40c7320b0cc6
Joshua Johnson <me@josh.land>
2015-04-10 00:32:46 -0700

    Running out of time to play with the render template ... removed. Added some pre styling

--- parse/utils.py
+++ parse/utils.py
@@ -1,6 +1,7 @@
 from library.feedparser import feedparser
 from copy import deepcopy
 
+
 # Parse a given feed and return a python dictionary with the feed results.
 def parse_feed(url):
     if url is not None:
@@ -40,7 +41,8 @@ def generate_media_details_from_rss(url):
 
                         # Bitrate options ... I already have the resolution and length
                         # Of the video file, without knowing the audio bitrate calculating may be innacurate.
-                        # I don't want to stream it for a short atoung of time and pull metadata from it.
+                        # I don't want to stream it for a short amount of time just to pull metadata from it.
+                        # A head request against the content url didn't return enough info to be able to discern.
 
 
                     if value == 0 or value == '' or value is None or value == "None":
